7fbd1924af81da9119d25747ed201f0df1cd1f34
Anoop Kumar <amt312002@gmail.com>
2024-05-20 16:37:40 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -27,7 +27,11 @@ app.use(cors({
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 })); 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
