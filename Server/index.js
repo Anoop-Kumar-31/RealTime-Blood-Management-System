feacf78ee906623e7fe5c994de1d6857b342500b
Anoop Kumar <amt312002@gmail.com>
2024-05-21 19:53:23 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -27,20 +27,20 @@ app.use(function(req, res, next) {
 
 
 
-// const allowedOrigins = ['https://bloodmanagementsystem-anoop.vercel.app', 'https://realtime-blood-management-system.onrender.com'];
-// app.use(cors({
-//   origin: function(origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: 'GET,POST,OPTIONS,PUT,PATCH,DELETE',
-//   allowedHeaders: 'X-Requested-With,content-type',
-//   credentials: true
-// }));
+const allowedOrigins = ['https://bloodmanagementsystem-anoop.vercel.app', 'https://realtime-blood-management-system.onrender.com'];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET,POST,OPTIONS,PUT,PATCH,DELETE',
+  allowedHeaders: 'X-Requested-With,content-type',
+  credentials: true
+}));
 
 
 
