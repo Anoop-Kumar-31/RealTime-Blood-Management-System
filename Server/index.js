87dede58a5f71c01c84cd6d12443fe2a1bbdbd83
Anoop Kumar <amt312002@gmail.com>
2024-05-21 19:39:04 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -20,7 +20,7 @@ const cors = require('cors');
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'application/json');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
