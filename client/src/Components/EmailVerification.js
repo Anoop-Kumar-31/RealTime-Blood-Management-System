c1446ce9894d4905795f3e17b51fb35b13699d36
Anoop Kumar <amt312002@gmail.com>
2024-08-01 15:53:12 +0530

    re

--- client/src/Components/EmailVerification.js
+++ client/src/Components/EmailVerification.js
@@ -18,7 +18,7 @@ const EmailVerification = (props) => {
   const handleSendOTP = async () => {
     try {
       // Make an API call to your server to send the OTP
-      const response = await fetch('/api/send-otp', {
+      const response = await fetch('https://bloodmanagementsystem-anoop.vercel.app/api/send-otp', {
         method: 'POST',
         mode: 'cors',
         headers: {
