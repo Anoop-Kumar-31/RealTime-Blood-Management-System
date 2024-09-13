2b86e5c272fd3b3d1832e5d35c29445a8b8949b5
Anoop Kumar <amt312002@gmail.com>
2024-09-13 09:53:52 +0530

    Update Form.js

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -39,7 +39,7 @@ export default function Form(){
 
         fetch(`https://realtime-blood-management-system.onrender.com/api/register`, {
             method: 'POST',
-            mode:'no-cors',
+            mode:'cors',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(valueDATA)
           })
